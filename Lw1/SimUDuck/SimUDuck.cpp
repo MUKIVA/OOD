5ef13c16a16d4ab7f9100b9fdbaf8c6efcd3382a
Van <mukiva2261@gmail.com>
2021-09-12 19:10:13 +0300

    Task1

---
+++ Lw1/SimUDuck/SimUDuck.cpp
@@ -0,0 +1,229 @@
+#include <cassert>
+#include <iostream>
+#include <memory>
+#include <vector>
+
+using namespace std;
+
+struct IFlyBehavior
+{
+	virtual ~IFlyBehavior(){};
+	virtual void Fly() = 0;
+};
+
+class FlyWithWings : public IFlyBehavior
+{
+public:
+	void Fly() override
+	{
+		cout << "I'm flying with wings!!" << endl;
+	}
+};
+
+class FlyNoWay : public IFlyBehavior
+{
+public:
+	void Fly() override {}
+};
+
+struct IQuackBehavior
+{
+	virtual ~IQuackBehavior(){};
+	virtual void Quack() = 0;
+};
+
+struct IDanceBehavior
+{
+	virtual ~IDanceBehavior() {};
+	virtual void Dance() = 0;
+};
+
+class DanceWaltz : public IDanceBehavior
+{
+	void Dance() override
+	{
+		cout << "I'm dance waltz!!" << endl;
+	}
+};
+
+class DanceMinuet : public IDanceBehavior
+{
+	void Dance() override
+	{
+		cout << "I'm dance minuet!!" << endl;
+	}
+};
+
+class DanceNoWay : public IDanceBehavior
+{
+	void Dance() override {}
+};
+
+class QuackBehavior : public IQuackBehavior
+{
+public:
+	void Quack() override
+	{
+		cout << "Quack Quack!!!" << endl;
+	}
+};
+class SqueakBehavior : public IQuackBehavior
+{
+public:
+	void Quack() override
+	{
+		cout << "Squeek!!!" << endl;
+	}
+};
+
+class MuteQuackBehavior : public IQuackBehavior
+{
+public:
+	void Quack() override {}
+};
+
+class Duck
+{
+public:
+	Duck(unique_ptr<IFlyBehavior>&& flyBehavior,
+		unique_ptr<IQuackBehavior>&& quackBehavior,
+		unique_ptr<IDanceBehavior>&& danceBehavior)
+		: m_quackBehavior(move(quackBehavior))
+	{
+		assert(m_quackBehavior);
+		SetFlyBehavior(move(flyBehavior));
+		SetDanceBehavior(move(danceBehavior));
+	}
+	void Quack() const
+	{
+		m_quackBehavior->Quack();
+	}
+	void Swim()
+	{
+		cout << "I'm swimming" << endl;
+	}
+	void Fly()
+	{
+		m_flyBehavior->Fly();
+	}
+	void Dance()
+	{
+		m_danceBehavior->Dance();
+	}
+	void SetFlyBehavior(unique_ptr<IFlyBehavior>&& flyBehavior)
+	{
+		assert(flyBehavior);
+		m_flyBehavior = move(flyBehavior);
+	}
+
+	void SetDanceBehavior(unique_ptr<IDanceBehavior>&& danceBehavior)
+	{
+		assert(danceBehavior);
+		m_danceBehavior = move(danceBehavior);
+	}
+	virtual void Display() const = 0;
+	virtual ~Duck() = default;
+
+private:
+	unique_ptr<IFlyBehavior> m_flyBehavior;
+	unique_ptr<IQuackBehavior> m_quackBehavior;
+	unique_ptr<IDanceBehavior> m_danceBehavior;
+};
+
+class MallardDuck : public Duck
+{
+public:
+	MallardDuck()
+		: Duck(make_unique<FlyWithWings>(), make_unique<QuackBehavior>(), make_unique<DanceWaltz>())
+	{
+	}
+
+	void Display() const override
+	{
+		cout << "I'm mallard duck" << endl;
+	}
+};
+
+class RedheadDuck : public Duck
+{
+public:
+	RedheadDuck()
+		: Duck(make_unique<FlyWithWings>(), make_unique<QuackBehavior>(), make_unique<DanceMinuet>())
+	{
+	}
+	void Display() const override
+	{
+		cout << "I'm redhead duck" << endl;
+	}
+};
+class DecoyDuck : public Duck
+{
+public:
+	DecoyDuck()
+		: Duck(make_unique<FlyNoWay>(), make_unique<MuteQuackBehavior>(), make_unique<DanceNoWay>())
+	{
+	}
+	void Display() const override
+	{
+		cout << "I'm decoy duck" << endl;
+	}
+};
+class RubberDuck : public Duck
+{
+public:
+	RubberDuck()
+		: Duck(make_unique<FlyNoWay>(), make_unique<SqueakBehavior>(), make_unique<DanceNoWay>())
+	{
+	}
+	void Display() const override
+	{
+		cout << "I'm rubber duck" << endl;
+	}
+};
+
+class ModelDuck : public Duck
+{
+public:
+	ModelDuck()
+		: Duck(make_unique<FlyNoWay>(), make_unique<QuackBehavior>(), make_unique<DanceNoWay>())
+	{
+	}
+	void Display() const override
+	{
+		cout << "I'm model duck" << endl;
+	}
+};
+
+void DrawDuck(Duck const& duck)
+{
+	duck.Display();
+}
+
+void PlayWithDuck(Duck& duck)
+{
+	DrawDuck(duck);
+	duck.Quack();
+	duck.Fly();
+	duck.Dance();
+	cout << endl;
+}
+
+int main()
+{
+	MallardDuck mallardDuck;
+	PlayWithDuck(mallardDuck);
+
+	RedheadDuck redheadDuck;
+	PlayWithDuck(redheadDuck);
+
+	RubberDuck rubberDuck;
+	PlayWithDuck(rubberDuck);
+
+	DecoyDuck decoyDuck;
+	PlayWithDuck(decoyDuck);
+
+	ModelDuck modelDuck;
+	PlayWithDuck(modelDuck);
+	modelDuck.SetFlyBehavior(make_unique<FlyWithWings>());
+	PlayWithDuck(modelDuck);
+}
